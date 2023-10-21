7251dab5ad41181fb2baf5a47e8ad546ffc38bf4
Qutaiba Qusai Makahleh <qutaiba.makahleh@gmail.com>
2023-10-21 08:31:12 +0300

    HW-1

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(const MyApp());
 }
 
+// StatelessWidget: 1part
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//StatefulWidget: 2part
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -35,36 +37,78 @@ class _MyHomePageState extends State<MyHomePage> {
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "1",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "2",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "3",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "4",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "5",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "6",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "7",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
             ],
           ),
@@ -74,36 +118,78 @@ class _MyHomePageState extends State<MyHomePage> {
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "8",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "9",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "10",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "11",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "12",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "13",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "14",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
             ],
           ),
@@ -113,36 +199,78 @@ class _MyHomePageState extends State<MyHomePage> {
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "15",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "16",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "17",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "18",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "19",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "20",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "21",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
             ],
           ),
@@ -152,36 +280,78 @@ class _MyHomePageState extends State<MyHomePage> {
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "22",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "23",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "24",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "25",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "26",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.black,
+                alignment: Alignment.center,
+                child: const Text(
+                  "27",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: MediaQuery.of(context).size.height / 7,
                 color: Colors.white,
+                alignment: Alignment.center,
+                child: const Text(
+                  "28",
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
             ],
           ),
